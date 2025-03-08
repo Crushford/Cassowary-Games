19af4cbf1a88f8220ed792cfa8cfe9c84cfa2570
James Rushford <jrushford@gitlab.com>
2025-03-08 17:53:03 +1100

    add tailwind but istnt workign

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,13 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import eslintPlugin from 'vite-plugin-eslint';
 import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    eslintPlugin({ failOnError: false })
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  }
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 });
