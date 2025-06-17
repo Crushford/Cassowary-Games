975099fc005b129a866fa17c9a00001f9d9c66ce
James Rushford (Personal) <rushford.james@gmail.com>
2025-06-17 13:44:43 +0200

    fix deployment (hopefully)

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,8 +2,16 @@ import { createRouter, createWebHistory } from 'vue-router';
 import GameMode from '../views/GameMode.vue';
 import LevelBuilder from '../views/LevelBuilder.vue';
 
+function getBaseUrl() {
+  if (process.env.CI_PAGES_URL) {
+    const url = new URL(process.env.CI_PAGES_URL);
+    return url.pathname.endsWith('/') ? url.pathname : `${url.pathname}/`;
+  }
+  return '/';
+}
+
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(getBaseUrl()),
   routes: [
     {
       path: '/',
